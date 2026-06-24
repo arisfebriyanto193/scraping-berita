2ba09941468b7da6a61ca3effe5c68bbbb0f583d
arisfebriyanto193 <78741085+arisfebriyanto193@users.noreply.github.com>
2026-06-24 09:58:16 +0700

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -141,8 +141,8 @@
 // }
 
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
-
+//#const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+const BASE_URL ="https://be-semantic.qbyte.web.id/py/";
 /**
  * POST /api/search/realtime
  *
