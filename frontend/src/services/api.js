384b5b3bcff15ad3e56175bd6eeb20e40cf92420
arisfebriyanto193 <arisfebriyanto193@gmail.com>
2026-06-24 11:13:03 +0700

    okok

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -142,7 +142,7 @@
 
 
 //#const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
-const BASE_URL ="https://be-semantic.qbyte.web.id/py/";
+const BASE_URL ="https://be-semantic.qbyte.web.id/py";
 /**
  * POST /api/search/realtime
  *
--- py/files/app.py
+++ py/files/app.py
@@ -362,4 +362,4 @@ def search_realtime():
 # ─────────────────────────────────────────────────────────────────
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5055)
